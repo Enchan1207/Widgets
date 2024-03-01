a16635c34ad26b6c9221f5a2bab4afcd699c39b6
Enchan <enchant0130@gmail.com>
2024-03-01 15:16:38 +0900

    [Update #18] media widget editor

--- widgetsb/Widgets/Media/Controller/MediaWidgetContentEditorViewController.swift
+++ widgetsb/Widgets/Media/Controller/MediaWidgetContentEditorViewController.swift
@@ -9,17 +9,70 @@ import Cocoa
 
 /// MediaWidgetContentの設定画面を提供するビューコントローラ
 class MediaWidgetContentEditorViewController: NSViewController {
+    
+    // MARK: - GUI components
+    
+    @IBOutlet weak var filePathField: NSTextField! {
+        didSet {
+            updateFilePathField()
+        }
+    }
+    
+    // MARK: - Properties
 
     private weak var mediaWidgetContent: MediaWidgetContent?
 
     override var nibName: NSNib.Name? { "MediaWidgetContentEditorView" }
     
+    // MARK: - Initializers
+    
     init(mediaWidgetContent: MediaWidgetContent? = nil) {
         self.mediaWidgetContent = mediaWidgetContent
         super.init(nibName: nil, bundle: nil)
+        self.mediaWidgetContent?.delegates.addDelegate(self)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    deinit {
+        self.mediaWidgetContent?.delegates.removeDelegate(self)
+    }
+    
+    // MARK: - GUI actions
+    
+    @IBAction func onClickBrowse(_ sender: Any) {
+        let openPanel = NSOpenPanel()
+        openPanel.canChooseFiles = true
+        openPanel.canChooseDirectories = false
+        openPanel.allowsMultipleSelection = false
+        let response = openPanel.runModal()
+        guard response == .OK, let selectedFileURL = openPanel.url else {return}
+        mediaWidgetContent?.mediaURL = selectedFileURL
+    }
+    
+    // MARK: - Private methods
+    
+    private func updateFilePathField(){
+        let mediaFilePath: String?
+        if #available(macOS 13.0, *) {
+            mediaFilePath = mediaWidgetContent?.mediaURL?.path()
+        } else {
+            mediaFilePath = mediaWidgetContent?.mediaURL?.path
+        }
+        filePathField.stringValue = mediaFilePath ?? "(media not set)"
+    }
+    
+}
+
+extension MediaWidgetContentEditorViewController: WidgetContentDelegate {
+    
+    func widget(_ widgetContent: WidgetContent, didChange keyPath: AnyKeyPath) {
+        // 変更内容がメディアURLのそれであることを確認
+        guard let widgetContent = widgetContent as? MediaWidgetContent,
+              keyPath == \MediaWidgetContent.mediaURL else {return}
+        updateFilePathField()
+    }
+    
 }
