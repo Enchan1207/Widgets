5d210dd03c12a8ec16c15183e78cc0405d8acece
Enchan <enchant0130@gmail.com>
2024-03-03 08:02:44 +0900

    [Modify #22] widget anchor editor screen

--- widgetsb/Preferences/Controller/Sub sheet/Subsheet pages/WidgetAnchorEditorViewController.swift
+++ widgetsb/Preferences/Controller/Sub sheet/Subsheet pages/WidgetAnchorEditorViewController.swift
@@ -7,7 +7,62 @@
 
 import Cocoa
 
+/// ウィジェットアンカーエディタ
 class WidgetAnchorEditorViewController: NSViewController {
+    
+    // MARK: - GUI components
+    
+    /// 水平方向アライメントセレクタ
+    @IBOutlet weak var horizontalAlignSelector: NSSegmentedControl!
+    
+    /// 水平方向アライメントラベル
+    @IBOutlet weak var horizontalAlignmentLabel: NSTextField!
+    
+    /// 垂直方向アライメントセレクタ
+    @IBOutlet weak var verticalAlignSelector: NSSegmentedControl!
+    
+    /// 垂直方向アライメントラベル
+    @IBOutlet weak var verticalAlignmentLabel: NSTextField!
+    
+    /// x方向インセットフィールド
+    @IBOutlet weak var insetXField: NSTextField! {
+        didSet {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            // formatter.allowsFloats = false
+            insetXField.formatter = formatter
+        }
+    }
+    
+    /// x方向単位セレクタ
+    @IBOutlet weak var insetXUnitSelector: NSComboBox! {
+        didSet {
+            insetXUnitSelector.usesDataSource = true
+            insetXUnitSelector.delegate = self
+            insetXUnitSelector.dataSource = self
+        }
+    }
+    
+    /// y方向インセットフィールド
+    @IBOutlet weak var insetYField: NSTextField! {
+        didSet {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            // formatter.allowsFloats = false
+            insetYField.formatter = formatter
+        }
+    }
+    
+    /// y方向単位セレクタ
+    @IBOutlet weak var insetYUnitSelector: NSComboBox! {
+        didSet {
+            insetYUnitSelector.usesDataSource = true
+            insetYUnitSelector.delegate = self
+            insetYUnitSelector.dataSource = self
+        }
+    }
+    
+    // MARK: - Properties
     
     private weak var widgetWindowState: WidgetWindowState?
     
@@ -16,6 +71,8 @@ class WidgetAnchorEditorViewController: NSViewController {
     /// デリゲート
     weak var delegate: WidgetAnchorEditorDelegate?
 
+    // MARK: - Initializers
+    
     init(widgetWindowState: WidgetWindowState? = nil) {
         self.widgetWindowState = widgetWindowState
         super.init(nibName: nil, bundle: nil)
@@ -30,7 +87,36 @@ class WidgetAnchorEditorViewController: NSViewController {
         widgetWindowState?.delegates.removeDelegate(self)
     }
     
+    // MARK: - View lifecycle
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - GUI actions
+    
+    @IBAction func didHorizontalSelectionChange(_ sender: Any) {
+        print(horizontalAlignSelector.selectedSegment)
+    }
+    
+    @IBAction func didVerticalSelectionChange(_ sender: Any) {
+        print(verticalAlignSelector.selectedSegment)
+    }
+    
+}
+
+extension WidgetAnchorEditorViewController: NSComboBoxDelegate {
+    
+}
+
+extension WidgetAnchorEditorViewController: NSComboBoxDataSource {
+    
+    func numberOfItems(in comboBox: NSComboBox) -> Int {
+        return 2
+    }
+    
+    func comboBox(_ comboBox: NSComboBox, objectValueForItemAt index: Int) -> Any? {
+        return ["px", "%"][nullable: index]
     }
     
 }
